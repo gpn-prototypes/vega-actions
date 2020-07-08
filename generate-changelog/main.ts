282d153d449993c35e57f18a6814eaa416ff4551
hitmanet <nathan200033@gmail.com>
2020-07-08 13:47:33 +0300

    fix(deploy): фикс деплоя

--- generate-changelog/main.ts
+++ generate-changelog/main.ts
@@ -3,10 +3,10 @@ import * as path from 'path';
 
 import * as core from '@actions/core';
 
-const lernaChangelog = path.resolve('node_modules/@gpn-prototypes/vega-changelog/bin/cli.js');
+const changelogPath = path.resolve('node_modules/@gpn-prototypes/vega-changelog/bin/cli.js');
 
 const exec = (cmd: string): string => cp.execSync(cmd).toString();
 
-const changelog = exec(`node ${lernaChangelog}`);
+const changelog = exec(`node ${changelogPath}`);
 
 core.setOutput('changelog', JSON.stringify(changelog));
